e839be16ebe4a6af6705fa4f4fe572be6f50a215
Miroslav <housera94@gmail.com>
2017-07-09 00:57:49 +0100

    Tweet and timer
    Define the main tweet function, plus a tweet timer

--- index.js
+++ index.js
@@ -4,3 +4,26 @@ const Twit = require('twit')
 const access = require('./access.js')
 
 const T = new Twit(access.config)
+
+// setInterval(PostRandomNumber, 1000*30)
+
+
+PostRandomNumber();
+
+function PostRandomNumber() {
+
+  const tweet = {
+    status: 'Testing out Node Twitter Bot Again'
+  }
+
+  T.post('statuses/update', tweet, tweeted);
+
+  function tweeted(err, data, response) {
+    if (err){
+      console.log(err);
+    } else {
+      console.log("Tweeted: "+data.text)
+      console.log("From User: "+data.user.screen_name)
+    }
+  }
+}
